02ac2f2ed224ff60beaad9b0cd89546296bf2540
man45045150 <man45045150@hotmail.co.th>
2018-07-08 16:23:48 +0700

    Sending Put Request

--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -9,9 +9,12 @@ export class ServerService {
   constructor(private http:Http) { }
   storeServers(servers:any[]){
     const headers = new Headers({'Content-Type':'application/json'});
-    return this.http.post(this.url,
-    servers,
-    {headers: headers });
+    // return this.http.post(this.url,
+    // servers,
+    // {headers: headers });
+    return this.http.put(this.url,
+      servers,
+      {headers: headers });
   }
   getServers(){
     return this.http.get(this.url);
