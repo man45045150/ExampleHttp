c64793492c45ab2359016a8dd3b4fde196ce623a
man45045150 <man45045150@hotmail.co.th>
2018-07-08 16:09:23 +0700

    Adjust Request Header

--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -1,4 +1,4 @@
-import { Http } from '@angular/http';
+import { Http,Headers } from '@angular/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -8,6 +8,9 @@ export class ServerService {
 
   constructor(private http:Http) { }
   storeServers(servers:any[]){
-    return this.http.post('https://udemy-ng-http-94332.firebaseio.com/data.json',servers);
+    const headers = new Headers({'Content-Type':'application/json'});
+    return this.http.post('https://udemy-ng-http-94332.firebaseio.com/data.json',
+    servers,
+    {headers: headers });
   }
 }
